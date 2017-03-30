236e70bbd4c05a27a6906821a4caace49186fb45
antlad <antlad@users.noreply.github.com>
2017-03-30 10:00:32 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 u_id=`id -u`
 g_id=`id -g`
-docker run -it --rm -p 5901:5901 -v /home/${USER}/boom:/home/dev vladtroinich/devenv bash -c "addgroup --gid ${g_id} ${USER} && useradd ${USER} -u ${u_id} -g ${g_id} --home /home/dev && su ${USER} -c \"export USER=${USER} && vncserver :1 -rfbport 5901 -geometry 1920x1080 -depth 24 && tail -F /home/dev/.vnc/*.log\" "
+port=5901
+docker run -it --rm -p $port:$port --net=bridge -v /home/${USER}:/home/dev varcalc_devenv bash -c "addgroup --gid ${g_id} ${USER} && useradd ${USER} -u ${u_id} -g ${g_id} --home /home/dev && su ${USER} -c \"export USER=${USER} && /opt/TurboVNC/bin/vncserver :2 -xstartup startxfce4 -rfbport $port -geometry 1920x1080 -depth 16 -3dwm && tail -F /home/dev/.vnc/*.log\" "
